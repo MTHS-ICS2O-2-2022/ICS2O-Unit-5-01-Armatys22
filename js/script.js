0a28b81a03836dc4b3159709689b332bfc1561d8
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-04-23 15:04:38 +0000

    first slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Dominik Armtys
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register(
+    "/ICS2O-Unit5-01-HTML-Slider-Example/sw.js",
+    {
+      scope: "/ICS2O-Unit5-01-HTML-Slider-Example/",
+    }
+  )
+}
+
+/**
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+}
+
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML =
+    "<p>Value is: " + slider.value + "</p>"
+}
