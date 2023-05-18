491f3dd8b0e35c4846e847e202def56e7f417757
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-05-18 16:53:58 +0000

    jhkhjkhgk

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Dominik Armtys
-// Created on: Sep 2020
+// Created by: Dominik Armatys
+// Created on: may 2023
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Check servie worker.
+ * This function generates a random number between 1 and 6
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register(
-    "/ICS2O-Unit5-01-HTML-Slider-Example/sw.js",
-    {
-      scope: "/ICS2O-Unit5-01-HTML-Slider-Example/",
-    }
-  )
-}
+const randomNumber = Math.floor(Math.random() * 6) + 1 // returns a random integer from 1 to 6 into variable "randomNumber"
 
 /**
  * This function updates the slider value.
  */
-function updateSliderValue(valueFromSlider) {
-  document.getElementById("slider-value").innerHTML = valueFromSlider
-}
-
-/**
- * This function displays the slider value.
- */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML =
-    "<p>Value is: " + slider.value + "</p>"
+  // input
+  const guessedNumber = parseFloat(
+    document.getElementById("guessed-number").value
+  )
+
+  // process
+  if (guessedNumber == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was " + randomNumber + " you got it!"
+  }
+
+  if (guessedNumber != randomNumber) {
+    document.getElementById("answer").innerHTML = "Wrong answer. Try again"
+  }
 }
